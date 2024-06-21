0ee8a35693e568d27af33a5238d704592ee90688
udayshankar754 <uday.naxtre@gmail.com>
2024-06-21 13:50:15 +0530

    xcel service added

---
+++ src/app/services/excel/excel.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import * as XLSX from 'xlsx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExcelService {
+
+  constructor() { }
+  public readExcelFile(file: File): Promise<any> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        const data = new Uint8Array(event.target.result);
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+
+        const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+        resolve({ workbook, worksheet, json });
+      };
+
+      reader.onerror = (error) => {
+        reject(error);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  }
+
+  public writeExcelFile(data: any[], filename: string): void {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { 'data': worksheet },
+      SheetNames: ['data']
+    };
+    XLSX.writeFile(workbook, filename);
+  }
+}
